74fc7c3d36658206b9cb99b8397fc15821fb23c0
Ciucu-Barcan Theodor-George <ciucubarcantheodor23@stud.ase.ro>
2026-05-15 07:55:29 +0300

    Step 1: Define Structures

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+// Step 1: Define Structures
+struct Book {
+    int isbn;
+    float price;
+};
+
+struct TreeNode {
+    Book info;
+    TreeNode* left;
+    TreeNode* right;
+    int balanceFactor;
+};
 
 int main() {
-    std::cout << "Hello World!" << std::endl;
     return 0;
 }
