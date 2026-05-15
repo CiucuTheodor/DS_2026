afcdbd591d039901c916fff0ffb94d145702337b
Ciucu-Barcan Theodor-George <ciucubarcantheodor23@stud.ase.ro>
2026-05-15 07:57:05 +0300

    Step 6: Rotations

--- main.cpp
+++ main.cpp
@@ -66,6 +66,43 @@ int balance_factor(TreeNode* root) {
     return tree_height(root->left) - tree_height(root->right);
 }
 
+// Step 6: Rotations
+TreeNode* rotate_left(TreeNode* x) {
+    TreeNode* y = x->right;
+    TreeNode* T2 = y->left;
+    
+    y->left = x;
+    x->right = T2;
+    
+    x->balanceFactor = balance_factor(x);
+    y->balanceFactor = balance_factor(y);
+    
+    return y;
+}
+
+TreeNode* rotate_right(TreeNode* y) {
+    TreeNode* x = y->left;
+    TreeNode* T2 = x->right;
+    
+    x->right = y;
+    y->left = T2;
+    
+    y->balanceFactor = balance_factor(y);
+    x->balanceFactor = balance_factor(x);
+    
+    return x;
+}
+
+TreeNode* rotate_left_right(TreeNode* root) {
+    root->left = rotate_left(root->left);
+    return rotate_right(root);
+}
+
+TreeNode* rotate_right_left(TreeNode* root) {
+    root->right = rotate_right(root->right);
+    return rotate_left(root);
+}
+
 int main() {
     return 0;
 }
